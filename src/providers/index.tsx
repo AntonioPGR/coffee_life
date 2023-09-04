4e24dac4a5eea08fca0bbe7d2fa8f3f787b95eb0
Antonio Pacheco Gerim Ribeiro <antoninhopgr@gmail.com>
2023-09-04 20:20:16 -0300

    RESTART

--- src/providers/index.tsx
+++
@@ -1,5 +0,0 @@
-import StyledComponentsProvider from "./StyledComponentsProvider";
-
-export default function Providers({ children }: PropsChildren) {
-	return <StyledComponentsProvider>{children}</StyledComponentsProvider>;
-}
