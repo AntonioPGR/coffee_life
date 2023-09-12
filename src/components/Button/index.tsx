f97bca7bc2dbebf58be389cbc5cac56f13dc0358
Antonio Pacheco Gerim Ribeiro <antoninhopgr@gmail.com>
2023-09-11 21:47:35 -0300

    Style

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -30,33 +30,10 @@ export default function Button({
 			{children}
 			{icon && (
 				<span className='flex items-center justify-center w-14 h-14 bg-bg-100 rounded-full'>
-					{button_animation && (
-						<motion.span
-							className='relative w-full h-full flex items-center justify-center'
-							whileHover={{
-								position: "relative",
-								left: ["0%", "10%", "20%", "30%", "-30%", "-20%", "-10%", "0%"],
-								top: ["0%", "-10%", "-20%", "-30%", "30%", "20%", "10%", "0%"],
-								scale: [1, 0.75, 0.5, 0.25, 0.25, 0.5, 0.75, 1],
-								opacity: [1, 0.8, 0.4, 0, 0, 0, 0.4, 0.8, 1],
-							}}
-							transition={{
-								duration: 2,
-								repeat: 0,
-							}}
-						>
-							<FontAwesomeIcon
-								className={"text-bg-800 w-3/5 h-3/5 " + iconClassName}
-								icon={icon}
-							/>
-						</motion.span>
-					)}
-					{!button_animation && (
-						<FontAwesomeIcon
-							className={"text-bg-800 w-3/5 h-3/5 " + iconClassName}
-							icon={icon}
-						/>
-					)}
+					<FontAwesomeIcon
+						className={"text-bg-800 w-3/5 h-3/5 " + iconClassName}
+						icon={icon}
+					/>
 				</span>
 			)}
 		</motion.button>
